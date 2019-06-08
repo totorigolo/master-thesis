fd1fdbd177a9211d53207b61a6a032d1258a617f
Thomas Lacroix <toto.rigolo@free.fr>
2019-06-08 10:24:45 +0200

    Add shebangs

--- compile-detailed.sh
+++ compile-detailed.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 pdflatex -synctex=1 main.tex \
     && makeglossaries main \
     && biber main \
--- compile-synthese.sh
+++ compile-synthese.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 pdflatex -synctex=1 synthese.tex \
     && biber synthese \
     && pdflatex -synctex=1 synthese.tex \
